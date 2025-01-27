dd8c7fccd2225b8c2d938a847e316ab3eb00a237
Tema <pshenichnykh.art@yandex.com>
2025-01-27 18:21:00 +0800

    add AssignmentService

---
+++ AssignmentService/Models/AssignmentModel.cs
@@ -0,0 +1,31 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace AssignmentService.Models
+{
+    public class AssignmentModel
+    {
+        public Guid Id { get; set; }
+        public string NameTask { get; set; }
+        public string DescriptionTask { get; set; }
+        
+        public Stack[] Stak { get; set; }
+        public int ExecutionTime {get;set; }
+
+        public enum Stack
+        {
+            None = 0,
+            JavaScript = 1,
+            TypeScript = 2,
+            React = 3,
+            NodeJS = 4,
+            Golang = 5,
+            Python = 6,
+            Java = 7,
+            CSharp = 8,
+            PHP = 9
+        }
+
+
+        
+    }
+}
---
+++ AssignmentService/Service/AssignmentService.cs
@@ -0,0 +1,57 @@
+﻿using AssignmentService.Models;
+using System.Collections.Generic;
+using static AssignmentService.Models.AssignmentModel;
+
+namespace AssignmentService.Service
+{
+    public class AssignmentService : IAssignmentService
+    {
+        List<AssignmentModel> assignments = new List<AssignmentModel>()
+        {
+            new AssignmentModel() { Id = Guid.NewGuid(), NameTask = "Тестовое задание1", DescriptionTask = "Описание тестового задания", Stak = [ Stack.TypeScript, Stack.React], ExecutionTime = 4 }
+        };
+        public AssignmentModel GetAssignment(Guid Id)
+        {
+            return assignments.FirstOrDefault(a => a.Id == Id);
+        }
+        public List<AssignmentModel> GetAll() { return assignments; }
+        public string AddAssignment(AssignmentModel assignment)
+        {
+            string result = string.Empty;
+            try
+            {
+                 assignments.Add(assignment);
+            }
+            catch (Exception ex) 
+            {
+                result = ex.Message;
+            }
+            return result == string.Empty ? $"Добавлено задание {assignment.NameTask}" : $"Ошибка - {result}";
+        }
+        public string RemoveAssignment(Guid Id)
+        {
+            string result = string.Empty;
+            try
+            {
+                assignments.Remove(GetAssignment(Id));
+            }
+            catch (Exception ex)
+            {
+                result = ex.Message;
+            }
+            return result == string.Empty ? $"Удалено задание номер: {Id.ToString()}" : $"Ошибка - {result}";
+        }
+        public AssignmentModel UpdateAssignment(AssignmentModel assignment)
+        {
+            AssignmentModel assignmentModel = assignments.FirstOrDefault(x=>x.Id == assignment.Id);
+
+            assignmentModel.Id = assignment.Id;
+            assignmentModel.NameTask = assignment.NameTask;
+            assignmentModel.DescriptionTask = assignment.DescriptionTask;
+            assignmentModel.Stak = assignment.Stak;
+            assignmentModel.ExecutionTime = assignment.ExecutionTime;
+
+            return assignmentModel;
+        }
+    }
+}
