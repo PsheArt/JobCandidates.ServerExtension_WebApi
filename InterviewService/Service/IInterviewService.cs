4e34b14d7aa8e20a2ca2a1e72303cbc465317f3b
Tema <pshenichnykh.art@yandex.com>
2025-02-06 17:45:28 +0800

    Modified InterviewService

--- InterviewService/Service/IInterviewService.cs
+++ InterviewService/Service/IInterviewService.cs
@@ -1,7 +1,19 @@
-﻿namespace InterviewService.Service
+﻿using AssignmentService.Models;
+using InterviewService.Models;
+
+namespace InterviewService.Service
 {
     public interface IInterviewService
     {
+        List<InterviewModel> Interviews();
+
+        InterviewModel GetInterview(Guid Id);
+        AssignmentModel GetAssignments(Guid Id);
+        string AddInterview(InterviewModel interview);
+        string UpdateInterview(InterviewModel interview);
+        string DeleteInterview(Guid Id);
+        string GetInteviewer(Guid Id);
+
 
     }
 }
