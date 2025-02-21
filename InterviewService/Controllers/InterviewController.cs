285660804b24d05393ecd2a54a397e129f4977b6
Tema <pshenichnykh.art@yandex.com>
2025-02-21 17:38:47 +0800

    Update InterviewController.cs

--- InterviewService/Controllers/InterviewController.cs
+++ InterviewService/Controllers/InterviewController.cs
@@ -13,7 +13,13 @@ namespace InterviewService.Controllers
         {
             this.InterviewService = interviewService;
         }
+        [HttpGet("GetInterviews")]
         public IActionResult GetInterviews() => Ok(InterviewService.Interviews());
+        [HttpGet("GetInterview_{Id}")]
+        public IActionResult GetInterview(Guid Id) => Ok(InterviewService.GetInterview(Id));
+        [HttpGet("GetInterviewer_{Id}")]
+        public IActionResult GetInterviewer(Guid Id) => Ok(InterviewService.GetInteviewer(Id));
+
         
     }
 }
